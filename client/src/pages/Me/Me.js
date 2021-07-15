ccbd09c5f455d7f9e2663771ad7c8d3ee3c8adfb
Michael Scharf <mwscharf@ucdavis.edu>
2021-07-15 13:07:33 -0700

    my issues page

--- client/src/pages/Me/Me.js
+++ client/src/pages/Me/Me.js
@@ -6,6 +6,9 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Icon from '@material-ui/core/Icon';
+import Issue from '../../components/Issue'
+import PubIssue from '../../components/PubIssue'
+
 
 const useStyles = makeStyles({
   root: {
@@ -42,25 +45,100 @@ const Me = () => {
 
   return (
     <>
-      <h1>Issues Reported by me</h1>
+      <h1 align="center">My Issues</h1>
       <div className='contactcontainer'>
         
             <Card className={classes.cardinfo}>
               <CardContent>
                 <Grid container>
-                  <Grid item xs={8}>
-                    <Typography variant="h5" component="h2" gutterBottom>
-                      Put stuff here
+                  <Grid item xs={12}>
+
+                
+
+                <Issue />
+              </Grid>
+            </Grid>
+
+            <br/>
+            
+
+
+            <Grid container>
+              <Grid item xs={12}>
+
+
+
+                <Issue />
+              </Grid>
+            </Grid>
+
+            <br/>
+
+            <Grid container>
+              <Grid item xs={12}>
+
+
+
+                <Issue />
+              </Grid>
+            </Grid>
+
+            <br />
+
+            <Grid container>
+              <Grid item xs={12}>
+
+
+
+                <Issue />
+              </Grid>
+            </Grid>
+
+            <br />
+
+            
+
+            
+
+                    {/* <Typography variant="h5" component="h2" gutterBottom className="center">
+                    Latest Issues
+                        
+                        <Card className={classes.cardinfo}>
+                    <CardContent>
+                      <Grid container>
+                        <Grid item xs={12}>
+
+                          <Typography variant="h5" component="h2" gutterBottom>
+                            Issue Title
+                    </Typography>
+                        </Grid>
+                        <Grid item xs={4}>
+                          <Typography variant="h5" component="h4" gutterBottom>
+                            Poster
+                          </Typography>
+
+                          <Typography variant="h5" component="h2" gutterBottom>
+                            Issue Body
+                    </Typography>
+                        </Grid>
+                      </Grid>
+                    </CardContent>
+                  </Card>
+
+                  <PubIssue />
+
+
                     </Typography>
                   </Grid>
                   <Grid item xs={4}>
-                    <Typography variant="h5" component="h2" gutterBottom>
+                    {/* <Typography variant="h5" component="h2" gutterBottom>
                       Put stuff here
-                    </Typography>
-                  </Grid>
-                </Grid>
+                    </Typography> */}
+                 
               </CardContent>
             </Card>
+
+
           
       </div>
     </>
