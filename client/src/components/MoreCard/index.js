7b649d3d0fcb9d10480f5b0caa9ee20325fb8472
diaseu <diana.seung@gmail.com>
2021-07-16 11:50:49 -0700

    made 'add project' card a separate component

---
+++ client/src/components/MoreCard/index.js
@@ -0,0 +1,1 @@
+export { default } from './MoreCard.js'
--- client/src/pages/Projects/Projects.js
+++ client/src/pages/Projects/Projects.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import ProjectCard from '../../components/ProjectCard'
+import MoreCard from '../../components/MoreCard'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Spacer from '../../components/Spacer'
@@ -83,16 +84,7 @@ const Projects = () => {
           <ProjectCard />
         </Grid>
         <Grid className={classes.projectcard} item xs={2}>
-          <Card className={classes.root}>
-            <CardContent>
-              <Typography variant="h6" component="h2">
-                +
-              </Typography>
-            </CardContent>
-            {/* <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions> */}
-          </Card>
+          <MoreCard />
         </Grid>
       </Grid>
       
