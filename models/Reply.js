9bd1832ede8a0a279d248818ab360710b3b29aa1
Jeckfeldt <jeckfeldt123@gmail.com>
2021-07-16 11:49:50 -0700

    finished reply model

--- models/Reply.js
+++ models/Reply.js
@@ -3,5 +3,10 @@ const { model, Schema} = require('mongoose')
 //just see the author of the reply
 const Reply = new Schema({
   text: String,
-  
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
+
+module.exports = model('Reply', Reply)
