3114b8ef286675a8b71fe6b6da93ac9e8022ffff
Jeckfeldt <jeckfeldt123@gmail.com>
2021-07-16 12:00:06 -0700

    finished issue model

--- models/Issue.js
+++ models/Issue.js
@@ -1,1 +1,19 @@
 const { model, Schema } = require('mongoose')
+
+//see author, see replies
+const Issue = new Schema({
+  title: String,
+  body: String,
+  isPublic: Boolean,
+  status: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  replies: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Reply'
+  }]
+})
+
+module.exports = model('Issue', Issue)
