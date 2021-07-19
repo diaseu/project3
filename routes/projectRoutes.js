d595d242e851858d926cc1b0049129b88a2ed7ce
Jeckfeldt <jeckfeldt123@gmail.com>
2021-07-19 11:08:57 -0700

    get project by id route

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,6 +2,26 @@ const router = require('express').Router()
 const { Issue, Reply, Project, User } = require('../models')
 const passport = require('passport')
 
+//get project by id get the info, owner, members, issues, and issues authors
+router.get('/projects/:id', passport.authenticate('jwt'), (req, res) => {
+  Project.findById(req.params.id)
+    .populate('owner')
+    .populate({
+      path: 'members',
+      model: 'User'
+    })
+    .populate({
+      path: 'issues',
+      model: 'Issue',
+      populate: {
+        path: 'author',
+        model: 'User'
+      }
+    })
+    .then(project => res.json(project))
+    .catch(err => console.log(err))
+})
+
 //create new project route
 router.post('/projects', passport.authenticate('jwt'), (req, res) => {
   Project.create({
@@ -21,6 +41,7 @@ router.post('/projects', passport.authenticate('jwt'), (req, res) => {
           })
         })
     })
+    .catch(err => console.log(err))
 })
 
 module.exports = router 
