081ffe6e74d2932214506abc48777d14a6397d95
Jeckfeldt <jeckfeldt123@gmail.com>
2021-07-20 21:44:44 -0700

    added update routes

--- routes/replyRoutes.js
+++ routes/replyRoutes.js
@@ -2,8 +2,7 @@ const router = require('express').Router()
 const { Issue, Reply, Project, User } = require('../models')
 const passport = require('passport')
 
-
-
+//make update, create, and delete no need for individual reply grabbing
 
 //create reply and push it to the issue replies
 router.post('/replies', passport.authenticate('jwt'), (req, res) => {
@@ -17,6 +16,12 @@ router.post('/replies', passport.authenticate('jwt'), (req, res) => {
         .then(() => res.json(reply))
         .catch(err => console.log(err))
     })
+    .catch(err => console.log(err))
+})
+
+router.put(`/replies/:id`, passport.authenticate('jwt'), (req, res) => {
+  Reply.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(project => res.json(project))
     .catch(err => console.log(err))
 })
 
